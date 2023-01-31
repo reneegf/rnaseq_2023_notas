be59539190f0152c274e28ac0a21a34750289358
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-01-31 13:33:58 -0600

    New adds packages

--- R/03-bioc-release.R
+++ R/03-bioc-release.R
@@ -1,11 +1,12 @@
 ### PanViz - Integrating Multi-Omic Network Data With Summay-Level GWAS Data
-#Integrates KEGG and GWAS data in order to produce integrates Multi-Omic Networks,
-#so it helps with the interpretability of GWAS data.class
-#It has 48 dependencies and it's rank 2141/2183, and it's available in all platforms
+# Integrates KEGG and GWAS data in order to produce integrates Multi-Omic Networks,
+# so it helps with the interpretability of GWAS data.class
+# It has 48 dependencies and it's rank 2141/2183, and it's available in all platforms
+# The vignette looks good, it doesn't look like a super complex package
 
 ### SPIAT - Spatial Image Analyisis of Tissues
-#It includes six analysiis modules that include visualization and categorization
-#of data generated from single-cell spatial proteomics plataforms
-#it has 192 dependencies and it's available in all platforms.
-
+# It includes six analysiis modules that include visualization and categorization
+# of data generated from single-cell spatial proteomics plataforms
+# It has 192 dependencies and it's available in all platforms.
+# It has 8 vignettes, it lokk like a really complex and complete package
 
