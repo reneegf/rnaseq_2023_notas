a1896b959bf751230721a739de7b99844a9a4ebe
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-01-31 13:24:29 -0600

    bioc release analysis

---
+++ R/03-bioc-release.R
@@ -0,0 +1,11 @@
+### PanViz - Integrating Multi-Omic Network Data With Summay-Level GWAS Data
+#Integrates KEGG and GWAS data in order to produce integrates Multi-Omic Networks,
+#so it helps with the interpretability of GWAS data.class
+#It has 48 dependencies and it's rank 2141/2183, and it's available in all platforms
+
+### SPIAT - Spatial Image Analyisis of Tissues
+#It includes six analysiis modules that include visualization and categorization
+#of data generated from single-cell spatial proteomics plataforms
+#it has 192 dependencies and it's available in all platforms.
+
+
