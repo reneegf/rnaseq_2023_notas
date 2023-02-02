bd40f3535176e19bc72ec56c21225cb96338119c
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-02-02 10:31:57 -0600

    ejercicios de modelos 5.1.4

---
+++ R/01-notas.R
@@ -0,0 +1,3 @@
+## En el ejerecicio de la clase, es treat pre - treat pos  when response resistant
+## Respecto al cero, se usa cuando tienes variables que no quieres tomar algo como
+#  referencia, por ejemplo cuando tienes batch
