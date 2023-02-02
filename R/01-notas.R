dc26326f6c677df286d5a6289b859481404ed1c9
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-02-02 11:45:17 -0600

    notas variables y limma-voom

--- R/01-notas.R
+++ R/01-notas.R
@@ -1,3 +1,9 @@
 ## En el ejerecicio de la clase, es treat pre - treat pos  when response resistant
 ## Respecto al cero, se usa cuando tienes variables que no quieres tomar algo como
 #  referencia, por ejemplo cuando tienes batch
+
+
+# la grafica de voom tiene un punto para cada gem , x es promedio de expresion del gen
+# y el eje y es varianza.
+
+#prenatalprenatal = (col)aprenatal(var)prenatal - (col)prenatal(var)postnatal
